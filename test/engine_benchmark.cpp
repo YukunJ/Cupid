dedc57c1ec3433e8243b5128731318cba7231db9
YukunJ <yukunj.cs@gmail.com>
2026-01-19 15:24:31 +0000

    feat: upload the 1 million trace and improve the test suite benchmark display
    2026-01-19T15:20:22+00:00
    Running ./engine_benchmark
    Run on (16 X 1000.24 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB (x8)
      L1 Instruction 32 KiB (x8)
      L2 Unified 1024 KiB (x8)
      L3 Unified 11264 KiB (x1)
    Load Average: 0.03, 0.04, 0.05
    --------------------------------------------------------------------------------------------------
    Benchmark                                        Time             CPU   Iterations UserCounters...
    --------------------------------------------------------------------------------------------------
    DefaultEngine/iterations:3/process_time       8813 ms         8813 ms            3 cancel_order=298.803k limit_order=701.217k memory_mb=45.7773 operations_per_second=37.8251k/s traces=1.00002M

---
+++ test/engine_benchmark.cpp
@@ -0,0 +1,95 @@
+#include <benchmark/benchmark.h>
+#include <cassert>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <numeric>
+#include <string>
+#include <vector>
+#include "default_engine.h"
+#include "engine_types.h"
+
+namespace cupid {
+
+enum class action_type : int8_t {
+  limit = 0,
+  cancel = 1,
+};
+
+struct benchmark_trace {
+  action_type action;
+  order_t order;
+  orderid_t cancel_id;
+
+  [[nodiscard]] constexpr bool is_limit() const noexcept { return action == action_type::limit; }
+
+  [[nodiscard]] constexpr bool is_cancel() const noexcept { return action == action_type::cancel; }
+};
+
+static std::vector<benchmark_trace> load_trace(const std::string &trace_path) {
+  std::ifstream trace_file(trace_path, std::ios::in | std::ios::binary);
+  if (!trace_file.is_open()) {
+    throw std::runtime_error("Failed to open trace file: " + trace_path);
+  }
+  std::vector<benchmark_trace> traces;
+  while (!trace_file.eof()) {
+    benchmark_trace trace;
+    trace_file.read(reinterpret_cast<char *>(&trace.action), sizeof(trace.action));
+    trace_file.read(reinterpret_cast<char *>(&trace.order.id), sizeof(trace.order.id));
+    trace_file.read(reinterpret_cast<char *>(&trace.order.px), sizeof(trace.order.px));
+    trace_file.read(reinterpret_cast<char *>(&trace.order.qty), sizeof(trace.order.qty));
+    trace_file.read(reinterpret_cast<char *>(&trace.order.side), sizeof(trace.order.side));
+    trace_file.read(reinterpret_cast<char *>(&trace.order.instr), INSTRUMENT_LEN);
+    trace_file.read(reinterpret_cast<char *>(&trace.order.trader), TRADER_LEN);
+    trace_file.read(reinterpret_cast<char *>(&trace.cancel_id), sizeof(trace.cancel_id));
+    if (!trace_file) {  // passed the last trace
+      break;
+    }
+    traces.push_back(trace);
+  }
+  trace_file.close();
+  return traces;
+}
+
+}  // namespace cupid
+
+// global default namespace
+
+const std::filesystem::path trace_path = std::filesystem::path(PROJECT_ROOT_PATH) / "1_million_trace.bin";
+
+template <typename EngineType>
+static void BM_Engine(benchmark::State &state) {  // NOLINT(runtime/references)
+  auto traces = cupid::load_trace(trace_path.string());
+  state.counters["traces"] = traces.size();
+  state.counters["limit_order"] = std::accumulate(
+      traces.begin(), traces.end(), 0ULL,
+      [](uint64_t accumulator, const auto &trace) { return accumulator + static_cast<uint64_t>(trace.is_limit()); });
+  state.counters["cancel_order"] = std::accumulate(
+      traces.begin(), traces.end(), 0ULL,
+      [](uint64_t accumulator, const auto &trace) { return accumulator + static_cast<uint64_t>(trace.is_cancel()); });
+  state.counters["memory_mb"] =
+      benchmark::Counter(static_cast<double>(traces.size()) * sizeof(cupid::benchmark_trace) / (1024.0 * 1024.0));
+  state.counters["operations_per_second"] =
+      benchmark::Counter(static_cast<double>(traces.size()), benchmark::Counter::kIsRate);
+
+  for (auto _ : state) {
+    EngineType engine;
+    for (const auto &trace : traces) {
+      if (trace.action == cupid::action_type::limit) {
+        engine.limit(trace.order);
+      } else if (trace.action == cupid::action_type::cancel) {
+        engine.cancel(trace.cancel_id);
+      } else {
+        assert(false);
+      }
+    }
+  }
+}
+
+BENCHMARK_TEMPLATE(BM_Engine, cupid::default_engine)
+    ->Name("DefaultEngine")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(3)
+    ->MeasureProcessCPUTime();
+
+BENCHMARK_MAIN();
